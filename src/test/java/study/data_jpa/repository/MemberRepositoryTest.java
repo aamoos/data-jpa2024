00d74a456070961f9b0db5bdf9a98a01e1d13a0c
kimjaesung <aamoos@naver.com>
2024-08-30 10:28:48 +0900

    스프링 데이터 JPA와 DB 설정, 동작확인

---
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package study.data_jpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.data_jpa.entity.Member;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        //원래 이렇게 쓰면 안됨 orElse 등 써서 해야함 예제라 그냥함
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
