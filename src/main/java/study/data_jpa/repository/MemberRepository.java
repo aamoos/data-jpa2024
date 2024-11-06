bcd6c61a816e215ba3f8809190a6ecb7ad54d834
kimjaesung <aamoos@naver.com>
2024-11-06 10:37:56 +0900

    Projections

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -63,5 +63,7 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Member> findLockByUsername(String username);
+
+    <T> List<NestedClosedProjections> findProjectionsByUsername(@Param("username") String username, Class<T> type);
 }
 
